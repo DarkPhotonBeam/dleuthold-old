25f15322e311a49fb82622e73a51d4b9d2094df9
Dan Photon <danphoton@yahoo.com>
2021-11-17 18:03:38 +0100

    Tools Link

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Layout from "../components/Layout/Layout";
 import {Helmet} from "react-helmet";
+import {Link} from 'gatsby';
 
 // markup
 export default function ToolsPage() {
@@ -9,6 +10,11 @@ export default function ToolsPage() {
             <Helmet title={'Tools - Daniel Leuthold'} />
             <section className={'center'}>
                 <h1>Tools</h1>
+                <ul className={'no-style spaced'}>
+                    <li>
+                        <Link to={'/tools/bqm-pitcher-calculator'}>bqm-pitcher-calculator</Link>
+                    </li>
+                </ul>
             </section>
         </Layout>
     )
