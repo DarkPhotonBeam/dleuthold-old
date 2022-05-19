2dea7730d88a585570ecf2b26a8f4f643969106e
Dan Photon <danphoton@yahoo.com>
2022-05-19 23:22:48 +0200

    Added hidden version marker

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ export default function IndexPage() {
     return (
         <Layout>
             <Helmet title={'Home - Daniel Leuthold'} />
+            <span className={'hidden'}>v1.0.1</span>
             <section className={'hero'}>
                 <h1>Hello.</h1>
             </section>
